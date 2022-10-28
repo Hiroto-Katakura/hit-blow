8aed78f93b5a26c09612a8b819f2374cd62e3aa6
Hiroto-Katakura <109775570+Hiroto-Katakura@users.noreply.github.com>
2022-10-28 13:10:58 +0900

    Add files via upload

--- page2.php
+++ page2.php
@@ -6,32 +6,39 @@
 </head>
 <body>
     <div class="top-wrapper">
-        <h1>Hit and Blow</h1>
-        <?php
+        <div class = "container">
+            <h1>Hit and Blow</h1>
+            <?php
 
-            //回数を数える
-            if(isset($_GET["kaisuu"])){
-            $num=$_GET["kaisuu"];
-            }else{
-            $num=1;
-            }
+                //回数を数える
+                if(isset($_GET["kaisuu"])){
+                $num=$_GET["kaisuu"];
+                }else{
+                $num=1;
+                }
 
-            if($num>1){
-            $co=$_GET["computer"]; 
-            }else{
-            $co=0;
-            }
+                if($num>1){
+                $co=$_GET["computer"]; 
+                }else{
+                $co=0;
+                }
 
-            echo "$num 回目";
-        ?>
+                echo "$num 回目";
+            ?>
 
-        <form action="./page3.php" method="GET">
-            <font color=red>重複しない0以外の4桁の数字を入力してください</font><br>
-            <input type="number" min="1234" max="9876" name="number"></br>
-            <input type="submit" value="決定">
-            <input type="hidden" value=<?php echo $num; ?>  name="kaisuu">
-            <input type="hidden" value=<?php echo $co; ?>  name="computer">
-        </form>
+            <form action="./page3.php" method="GET">
+                <font color=red>重複しない0以外の4桁の数字を入力してください</font><br>
+                <input type="number" min="1234" max="9876" name="number"></br>
+                <input class="bt" type="submit" value="決定">
+                <input type="hidden" value=<?php echo $num; ?>  name="kaisuu">
+                <input type="hidden" value=<?php echo $co; ?>  name="computer">
+            </form>
+        </div>
     </div>
+    <footer>
+        <div class="container">
+            <p>webアプリ構築実験</p>
+        </div>
+    </footer>
 </body>
 </html>
--- page3.php
+++ page3.php
@@ -6,64 +6,71 @@
 </head>
 <body>
     <div class="top-wrapper">
-        <h1>Hit and Blow</h1>
+        <div class = "container">
+            <h1>Hit and Blow</h1>
 
-        <?php
-        $p_num=$_GET['number'];
-        $num=$_GET['kaisuu'];
-        $hit=0;
-        $blow=0;
+            <?php
+            $p_num=$_GET['number'];
+            $num=$_GET['kaisuu'];
+            $hit=0;
+            $blow=0;
 
-        $com=array();
+            $com=array();
 
-        if($num==1){
-            $n=array("1","2","3","4","5","6","7","8","9");
-            shuffle($n);
-            for($i=0;$i<4;$i++){
-                    $com[$i]=$n[$i];
+            if($num==1){
+                $n=array("1","2","3","4","5","6","7","8","9");
+                shuffle($n);
+                for($i=0;$i<4;$i++){
+                        $com[$i]=$n[$i];
+                }
+                $co=implode("",$com);
+            }else{
+	            if(isset($_GET["computer"])){
+	                $co=$_GET["computer"];
+	                $com=str_split($co);
+	            }
             }
-            $co=implode("",$com);
-        }else{
-	        if(isset($_GET["computer"])){
-	            $co=$_GET["computer"];
-	            $com=str_split($co);
-	        }
-        }
 
-        $player=str_split($p_num);
-        echo $p_num;
-        echo '<br>';
+            $player=str_split($p_num);
+            echo $p_num;
+            echo '<br>';
 
-        for($i=0;$i<4;$i++){
-            for($y=0;$y<4;$y++){
-                if($com[$i]==$player[$y]){
-                    $hit+=1;
+            for($i=0;$i<4;$i++){
+                for($y=0;$y<4;$y++){
+                    if($com[$i]==$player[$y]){
+                        $hit+=1;
+                    }
                 }
             }
-        }
-        echo "hit:{$hit}";
-        echo '<br>';
+            echo "hit:{$hit}";
+            echo '<br>';
 
-        for($i=0;$i<4;$i++){
-            if($com[$i]==$player[$i]){
-                $blow+=1;
+            for($i=0;$i<4;$i++){
+                if($com[$i]==$player[$i]){
+                    $blow+=1;
+                }
             }
-        }
-        echo "blow:{$blow}";
-        echo '<br>';
+            echo "blow:{$blow}";
+            echo '<br>';
 
-        $num=$num+1;
-        ?>
+            $num=$num+1;
+            ?>
 
-        <?php if($blow==4): ?>
-            <input type="button" onclick="location.href='page4.html'" value="次に進む">
-        <?php else: ?>
-            <form action="./page2.php" method="GET">
-                <input type="submit" value="次に進む">
-                <input type="hidden" value=<?php echo $num; ?>  name="kaisuu">
-                <input type="hidden" value=<?php echo $co; ?>  name="computer">
-            </form>
-        <?php endif; ?>
+            <?php if($blow==4): ?>
+                <input class="bt1" type="button" onclick="location.href='page4.html'" value="次に進む">
+            <?php else: ?>
+                <form action="./page2.php" method="GET">
+                    <input class="bt2" type="submit" value="次に進む">
+                    <input type="hidden" value=<?php echo $num; ?>  name="kaisuu">
+                    <input type="hidden" value=<?php echo $co; ?>  name="computer">
+                </form>
+            <?php endif; ?>
+        </div>
     </div>
+    <footer>
+        <div class="container">
+            <p>webアプリ構築実験</p>
+        </div>
+    </footer>
 </body>
 </html>
